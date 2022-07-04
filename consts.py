323398da5d6dbd6230801bfaa4c9f4f49d8966a3
roditi <yuval.roditi@mail.huji.ac.il>
2022-07-04 16:17:49 +0300

    add consts

--- consts.py
+++ consts.py
@@ -4,17 +4,18 @@ ICD_CODES = ["99592", # Severe sepsis, icd_version =  9
              "R6521",  # Severe sepsis with septic shock, icd_version = 10
              "99591" # Sepsis
              ]
-CHARTEVENTS_CODES = [225309,
-                    225310,
-                    225312,
-                    220050,
-                    220051, 
-                    220052, 
-                    220179, 
-                    220180, 
-                    220181, 
-                    220045 #HR
-                    ] 
+BP = [#225309, # ART BP Systolic
+    #225310, # ART BP Diastolic
+    225312, # ART BP Mean
+    #220050, # Arterial Blood Pressure systolic
+    #220051, # Arterial Blood Pressure diastolic
+    220052, # Arterial Blood Pressure mean	
+    #220179, # Non Invasive Blood Pressure systolic
+    #220180, # Non Invasive Blood Pressure diastolic
+    220181 # Non Invasive Blood Pressure mean
+    ]
+HR = [220045] # Heart Rate
+CHARTEVENTS_CODES = BP + HR
 INPUTEVENTS_CODES = [
     221662, # Dopamine
     221289,	#Epinephrine
