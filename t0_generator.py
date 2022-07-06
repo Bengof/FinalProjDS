01c7c688ea9c91f15f2d17982f793c00a955d382
Bengof <ben.gofrit@mail.huji.ac.il>
2022-07-06 10:41:12 +0300

    add endtime to get_t0

--- t0_generator.py
+++ t0_generator.py
@@ -12,8 +12,8 @@ def get_t0():
     no_overlap_stay_ids = get_filtered_stay_ids()
     df_inputs = df_inputs[df_inputs["stay_id"].isin(no_overlap_stay_ids["stay_id"])]
     df_inputs = df_inputs[df_inputs["itemid_label"] == "Norepinephrine"]
-    df_inputs_changed = df_inputs[df_inputs["statusdescription"] == "ChangeDose/Rate"][["stay_id", "subject_id", "starttime"]].sort_values(by="starttime")
-    df_inputs_paused = df_inputs[df_inputs["statusdescription"] == "Paused"][["stay_id", "subject_id", "starttime"]].sort_values(by="starttime")
-    df_inputs_stopped = df_inputs[df_inputs["statusdescription"] == "Stopped"][["stay_id", "subject_id", "starttime"]].sort_values(by="starttime")
+    df_inputs_changed = df_inputs[df_inputs["statusdescription"] == "ChangeDose/Rate"][["stay_id", "subject_id", "starttime", "endtime"]].sort_values(by="starttime")
+    df_inputs_paused = df_inputs[df_inputs["statusdescription"] == "Paused"][["stay_id", "subject_id", "starttime", "endtime"]].sort_values(by="starttime")
+    df_inputs_stopped = df_inputs[df_inputs["statusdescription"] == "Stopped"][["stay_id", "subject_id", "starttime", "endtime"]].sort_values(by="starttime")
     return df_inputs_changed, df_inputs_paused, df_inputs_stopped
  
