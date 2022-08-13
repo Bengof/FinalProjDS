81c3f9edd593bdcefff7684a55d032065fe2422a
roditi <yuval.roditi@mail.huji.ac.il>
2022-08-13 23:41:09 +0300

    change parameters for get_relevant_doses

--- bp_for_dose.py
+++ bp_for_dose.py
@@ -1,7 +1,8 @@
 import inputs_states_filters
 import pandas as pd
 import numpy as np
-from preceding_events import get_events_beofore_dose, add_prev_decision
+from preceding_events import get_events_beofore_dose, get_prev_dose
+from states_creator import create_states
 import consts
 
 def get_nearest_bp(dose_row, chartevent):
@@ -19,16 +20,13 @@ def get_nearest_bp(dose_row, chartevent):
         return [np.nan, np.nan]
     return bp_event.to_list()
 
-def get_relevant_doses_with_bp(chartevent):
-    # get input events with states for each row if it is relevant and the reason for irrelevant:
-    inputevents_states = inputs_states_filters.get_inputs_with_states()
+def get_relevant_doses_with_bp(inputevents, chartevent):
+    inputevents_states = create_states(inputevents)
     # keep only events with no overlaps and other problems:
     inputevents_states = inputevents_states[inputevents_states["state"] == inputs_states_filters.State.OK]
     inputevents_states = inputevents_states[["stay_id","starttime","endtime", "rate", "statusdescription", "originalrate", "itemid_label", "state"]].sort_values(by=["stay_id","starttime"])
     # add to each row the prev_starttime:
-    inputevents_states = add_prev_decision(inputevents_states)
+    inputevents_states = get_prev_dose(inputevents_states)
     # select to each dose what is the closest BP events in the interval
     inputevents_states[["bp_time","bp_val"]] = inputevents_states.apply(lambda row: get_nearest_bp(row, chartevent), axis=1, result_type="expand")
-    # filter non Norepinephrine medicine
-    inputevents_states = inputevents_states[inputevents_states.itemid_label == "Norepinephrine"]
     return inputevents_states
