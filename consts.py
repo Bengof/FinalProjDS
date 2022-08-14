0e7dde175940df075d5684216c20d123d198be8c
Bengof <ben.gofrit@mail.huji.ac.il>
2022-08-14 11:44:05 +0300

    refactor

--- consts.py
+++ consts.py
@@ -67,5 +67,5 @@ HOURS_BEFORE_DOSE = 1
 BP_RANGES = ((0,44),(45,49),(50,54),(55,59),(60,64),(65,69),(70,74),(75,79),(80,84),(85,89),(90,94),(95,99),(100,104),(105,109),(110,114),(115,200))
 
 # minimal gap between dose to previous dose in order to mark it as dose which is not a decision in Pause and Stopped statusdescription
-MINIMAL_GAP = 2
+MINIMAL_GAP_MINUTS = 2
 
